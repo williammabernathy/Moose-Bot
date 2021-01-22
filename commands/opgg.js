de4ff4e836f7dbbcbb09f80b628ef6ba888887e8
Will Abernathy <matthew.abernathy@hotmail.com>
2021-01-22 11:00:30 -0600

    added opgg command functionality

--- commands/opgg.js
+++ commands/opgg.js
@@ -1,7 +1,16 @@
 module.exports = {
-    name: 'opgg',                   // command
-    description: 'Link MooseRx opgg account.',
-    execute(msg, args) {            // (msg: the entire message as a string as it was sent, args: array of args via parsed msg first position containing the command followed by preceeding words)
-      msg.channel.send('https://na.op.gg/summoner/userName=MooseRx');
-    },
-  };
+  name: "opgg", // command
+  description: "Link MooseRx opgg account.",
+  execute(msg, args) {
+    // (msg: the entire message as a string as it was sent, args: array of args via parsed msg first position containing the command followed by preceeding words)
+    // default no args, send MooseRx
+    if(args === null || args.length == 0) {
+      msg.channel.send("https://na.op.gg/summoner/userName=MooseRx");
+    }
+    // otherwise, link arg username
+    else {
+      console.info(`Joined args: ${args.join("+")}`);
+      msg.channel.send(`https://na.op.gg/summoner/userName=${args.join("+")}`);
+    }
+  },
+};
--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',                   // command
     description: 'Ping!',
     execute(msg, args) {            // (msg: the entire message as a string as it was sent, args: array of args via parsed msg first position containing the command followed by preceeding words)
-      msg.channel.send('pong');
+      msg.channel.send('Pong! I can hear you!');
     },
   };
