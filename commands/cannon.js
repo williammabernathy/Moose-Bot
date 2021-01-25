7ab06e6783232b11464b5ca14db1dd90672c4f60
Will Abernathy <matthew.abernathy@hotmail.com>
2021-01-25 12:43:02 -0600

    simple sqlit database and cannon command interaction setup

--- commands/cannon.js
+++ commands/cannon.js
@@ -1,7 +1,23 @@
 module.exports = {
-    name: 'cannon',                     // command
-    description: 'Count cannnon minions missed.',
-    execute(msg, args) {                // (msg: the entire message as a string as it was sent, args: array of args via parsed msg first position containing the command followed by preceeding words)
-      msg.channel.send('Missed a cannon');
-    },
-  };
+  name: 'cannon',                                         // command
+  description: 'Count cannnon minions missed.',
+  execute(msg, args) {                                    // (msg: the entire message as a string as it was sent, args: array of args via parsed msg first position containing the command followed by preceeding words)
+    const Tags = require("./index.js");
+
+    if (Tags === null) 
+    {
+      console.info('Tags is null');
+    }
+
+    incrCannonCount();
+
+    async function incrCannonCount() {
+      const tag = await Tags.findOne({ where: { name: 'cannon' } });
+      if (tag) {
+        tag.increment('cannons_missed');
+        return msg.channel.send(`Moose has missed ${tag.get('cannons_missed')} cannons ):`);
+      }
+    }
+
+  },
+};
--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-const { prefix, token } = require("./config.json");             // include config file containing tokens
+const { prefix, token } = require("./config.json");                     // include config file containing tokens
 
-const Discord = require("discord.js");                          // require discord.js library (.py for python bots)
+const Discord = require("discord.js");                                  // require discord.js library (.py for python bots)
+const Sequelize = require("sequelize");                                 // database package
 
-const bot = new Discord.Client();                               // build the bot client
+const bot = new Discord.Client();                                       // build the bot client
 bot.commands = new Discord.Collection();
 
-const botCommands = require("./commands");                      // get and require our command folder
+const botCommands = require("./commands");                              // get and require our command folder
 
 // map all commands inside ./commands directory
 Object.keys(botCommands).map((key) => {
   bot.commands.set(botCommands[key].name, botCommands[key]);
 });
 
-bot.login(token);
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite",
+});
 
-// message displayed to console after successful login (can be removed)
-bot.on("ready", () => {
+// create database to increment and track !cannon command
+const Tags = sequelize.define("cannon", {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  cannons_missed: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+});
+
+bot.once("ready", () => {
+  Tags.sync();
   console.info(`Logged in as ${bot.user.tag}!`);
 });
+
+async function addMoose() {
+  try {
+    const tag = await Tags.create({
+      name: "MooseRx",
+      cannons_missed: 0,
+    });
+  }
+  catch (e) {
+    if (e.name === 'SequelizeUniqueConstraintError') {
+      console.info('That tag already exists.');
+    }
+    console.info('Something went wrong with adding a tag.');
+  }
+}
+
+addMoose();
 
 // get the most recently typed message and parse
 bot.on("message", (msg) => {
@@ -26,22 +62,36 @@ bot.on("message", (msg) => {
     msg.pin();
   }
 
+  if (msg.content.toLowerCase() === '!cannon') {
+    incrCannonCount();
+
+    async function incrCannonCount() {
+      const tag = await Tags.findOne({ where: { name: 'MooseRx' } });
+      if (tag) {
+        tag.increment('cannons_missed');
+        return msg.channel.send(`Moose has missed ${tag.get('cannons_missed')} cannons ):`);
+      }
+    }
+  }
+
   if (msg.channel.name.toLowerCase() !== "moose-bot-spam") return;
 
   if (msg.content[0] !== prefix) return;
 
-  const args = msg.content.split(/ +/);                         // split into array based on spaces
-  const command = args.shift().toLowerCase();                   // command from array to lower case
-  console.info(`Called command: ${command.substring(1)}`);      // print command to console (can be removed or commented out)
+  const args = msg.content.split(/ +/);                                 // split into array based on spaces
+  const command = args.shift().toLowerCase();                           // command from array to lower case
+  console.info(`Called command: ${command.substring(1)}`);              // print command to console (can be removed or commented out)
 
-  if (!bot.commands.has(command.substring(1))) return;          // does a command exist from Object mapping at line ~9 (bot.commands.set)
+  if (!bot.commands.has(command.substring(1))) return;                  // does a command exist from Object mapping at line ~9 (bot.commands.set)
 
   // if command exist, try executing, passing the msg and args array parameters
   // command execution in ./commands/<command>.js file, where the command is the typed and parsed command
   try {
-    bot.commands.get(command.substring(1)).execute(msg, args);
+    //bot.commands.get(command.substring(1)).execute(msg, args);
   } catch (error) {
     console.error(error);
     msg.reply("Oops! Something went wrong trying to execute that command.");
   }
 });
+
+bot.login(token);
